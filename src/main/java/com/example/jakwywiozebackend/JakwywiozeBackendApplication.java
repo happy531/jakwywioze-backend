7998b62df7d036ff269e8b4b91231d4abf01b827
happy531 <jedrzejwesolowski177@gmail.com>
2023-04-20 14:13:45 +0200

    Init backend

---
+++ src/main/java/com/example/jakwywiozebackend/JakwywiozeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jakwywiozebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakwywiozeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakwywiozeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jakwywiozebackend/JakwywiozeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jakwywiozebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakwywiozeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
