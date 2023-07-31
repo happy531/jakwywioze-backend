19ef891fc1240f59b4b5a5b777fa8bab44b027a9
Igor Mianowany <fimiany@gmail.com>
2023-07-31 14:11:02 +0200

    Alternative version

--- src/main/java/com/example/jakwywiozebackend/service/impl/PointServiceImpl.java
+++ src/main/java/com/example/jakwywiozebackend/service/impl/PointServiceImpl.java
@@ -179,6 +179,12 @@ public class PointServiceImpl implements PointService {
 
     @Override
     public List<PointDto> getFilteredPoints(FilterInfoDto filterInfoDto) {
+        if(true){
+            Specification<Point> spec = Specification.where(PointSpecification.getPointByCity(filterInfoDto.getCity()))
+                    .and(PointSpecification.getPointByWasteTypes(filterInfoDto.getWasteTypesNames()));
+            List<Point> points = pointRepository.findAll(spec);
+            return pointMapper.toPointDtoList(points);
+        }
         if(filterInfoDto.getCity().isEmpty() && filterInfoDto.getWasteTypesNames().isEmpty()){
             return getPoints();
         }
