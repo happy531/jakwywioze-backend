c1578b2b831dbd07b2ff4c6e412801d4fb15ca26
happy531 <jedrzejwesolowski177@gmail.com>
2023-08-17 12:00:18 +0200

    [JAK-78] Filtrowanie po range

--- src/main/java/com/example/jakwywiozebackend/service/impl/PointServiceImpl.java
+++ src/main/java/com/example/jakwywiozebackend/service/impl/PointServiceImpl.java
@@ -4,14 +4,12 @@ import com.example.jakwywiozebackend.dto.CityDto;
 import com.example.jakwywiozebackend.dto.FilterInfoDto;
 import com.example.jakwywiozebackend.dto.PointDto;
 import com.example.jakwywiozebackend.dto.WasteTypeDto;
-import com.example.jakwywiozebackend.entity.City;
 import com.example.jakwywiozebackend.entity.Point;
 import com.example.jakwywiozebackend.entity.WasteType;
 import com.example.jakwywiozebackend.mapper.PointMapper;
 import com.example.jakwywiozebackend.mapper.WasteTypeMapper;
 import com.example.jakwywiozebackend.repository.PointRepository;
 import com.example.jakwywiozebackend.repository.WasteTypeRepository;
-import com.example.jakwywiozebackend.service.CityService;
 import com.example.jakwywiozebackend.service.PointService;
 import com.example.jakwywiozebackend.service.PointSpecification;
 import com.example.jakwywiozebackend.utils.Utils;
@@ -23,6 +21,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 @Service
@@ -48,9 +47,9 @@ public class PointServiceImpl implements PointService {
     @Override
     public PointDto createPoint(PointDto pointDto) {
         List<PointDto> points = getPoints();
-        for (PointDto p: points) {
-            if(p.getLon() == pointDto.getLon()
-                    && p.getLat() == pointDto.getLat()){
+        for (PointDto p : points) {
+            if (p.getLon() == pointDto.getLon()
+                    && p.getLat() == pointDto.getLat()) {
                 throw new EntityExistsException("Point already exists");
             }
         }
@@ -63,13 +62,13 @@ public class PointServiceImpl implements PointService {
         List<WasteType> wasteTypes = point.getWasteTypes();
         // only god can judge me
         int i = 0;
-        while (i < wasteTypes.size()){
-            if(wasteTypes.get(i).getName().equals(wasteTypeDto.getName())) {
+        while (i < wasteTypes.size()) {
+            if (wasteTypes.get(i).getName().equals(wasteTypeDto.getName())) {
                 return pointMapper.toPointDto(pointRepository.save(point));
             }
             i++;
         }
-        if(wasteTypeRepository.findByName(wasteTypeDto.getName()).isPresent()){
+        if (wasteTypeRepository.findByName(wasteTypeDto.getName()).isPresent()) {
             wasteTypes.add(wasteTypeRepository.findByName(wasteTypeDto.getName()).get());
             return pointMapper.toPointDto(pointRepository.save(point));
         }
@@ -87,7 +86,7 @@ public class PointServiceImpl implements PointService {
         CityDto city = cityService.getCityByName(filterInfoDto.getCity());
         List<Point> pointsInRange = Utils.filterPointsByRange(pointWithoutRange, city, filterInfoDto.getRange());
         Pageable pageable = PageRequest.of(filterInfoDto.getPage(), filterInfoDto.getItemsPerPage());
-        Page<Point> pointsPage = pointRepository.findAll(pointWithoutRange, pageable);
+        Page<Point> pointsPage = pointRepository.findAll(pointsInRange, pageable);
         List<Point> points = pointsPage.getContent();
         return pointMapper.toPointDtoList(points);
     }
