91ab03b81994e58d7c78b7de13db546534648904
Igor Mianowany <fimiany@gmail.com>
2023-12-01 16:50:50 +0100

    Added mail template

--- src/main/java/com/example/jakwywiozebackend/utils/Listeners/RegistrationListener.java
+++ src/main/java/com/example/jakwywiozebackend/utils/Listeners/RegistrationListener.java
@@ -3,14 +3,13 @@ package com.example.jakwywiozebackend.utils.Listeners;
 import com.example.jakwywiozebackend.entity.User;
 import com.example.jakwywiozebackend.service.UserService;
 import com.example.jakwywiozebackend.utils.Events.OnRegistrationCompleteEvent;
+import com.example.jakwywiozebackend.utils.Utils;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
@@ -24,7 +23,6 @@ public class RegistrationListener implements
         ApplicationListener<OnRegistrationCompleteEvent> {
 
     private final UserService userService;
-    private final JavaMailSender mailSender;
 
 
     @Override
@@ -38,33 +36,13 @@ public class RegistrationListener implements
         userService.createVerificationTokenForUser(user, token);
 
         String recipientAddress = user.getEmail();
-        String subject = "Registration Confirmation";
+        String subject = "Jakwywioze potwierdzenie rejestracji";
         String confirmationUrl = BASE_FRONTEND_URL + "/users/confirm-registration?token=" + token;
 
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo(recipientAddress);
-        email.setSubject(subject);
-//        email.setText(confirmationUrl);
-//        mailSender.send(email);
-
-        email.setText("<h2>Reset password request </h2>" +
-                "<h3> Please click on the button to reset password </h3>"+
-                "<a target='_blank' href=" + confirmationUrl + "> <button>Reset your password</button></a>"
-        );
-
-
-
-
-        String to = "fimiany@gmail.com";
-        //provide sender's email ID
-        String from = "jakwywioze@gmail.com";
-        //provide Mailtrap's username
+        String from = "jakwywioze.pl";
         final String username = System.getenv("EMAIL");
-        //provide Mailtrap's password
         final String password = System.getenv("PASSWORD");
-        //provide Mailtrap's host address
         String host = "smtp.gmail.com";
-        //configure Mailtrap's SMTP server details
         Properties props = new Properties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
@@ -80,31 +58,16 @@ public class RegistrationListener implements
                     }
                 });
         try {
-            //create a MimeMessage object
             Message message = new MimeMessage(session);
-            //set From email field
             message.setFrom(new InternetAddress(from));
-            //set To email field
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            //set email subject field
-            message.setSubject("Here comes Jakarta Mail!");
-            //set the content of the email message
-//            message.setContent("<h2>Register confirmation </h2>" +
-//                    "<h3> Please click on the button to confirm registration </h3>"+
-//                    "<a target='_blank' href=" + confirmationUrl + "> <button>Confirm registration</button></a>",
-//                    "text/html; charset=utf-8"
-//            );
-            message.setContent("<a class=\"hover-like-lightbox mockup-shadow\" href=\""+ confirmationUrl +"\" rel=\"nofollow\" target=\"_blank\">\n" +
-                            "<picture>\n" +
-                            "<source srcset=\"https://images01.nicepagecdn.com/page/49/98/html-template-spreview-499811.webp\" type=\"image/webp\" media=\"(max-width: 450px)\">\n" +
-                            "<source srcset=\"https://images01.nicepagecdn.com/page/49/98/html-template-spreview-499811.jpg\" type=\"image/jpeg\" media=\"(max-width: 450px)\">\n" +
-                            "<source srcset=\"https://images01.nicepagecdn.com/page/49/98/html-template-preview-499811.webp\" type=\"image/webp\">\n" +
-                            "<source srcset=\"https://images01.nicepagecdn.com/page/49/98/html-template-preview-499811.jpg\" type=\"image/jpeg\">\n" +
-                            "<img alt=\"Text and large button HTML Template\" class=\"media-image img-responsive noversion\" height=\"306\" src=\"https://images01.nicepagecdn.com/page/49/98/html-template-preview-499811.jpg\" style=\"\" width=\"750\">",
-                    "text/html; charset=utf-8"
-            );
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipientAddress));
+            message.setSubject(subject);
+            String content = Utils.getMailHtml(
+                    "Dziękujemy za założenie konta na portalu Jakwywioze.pl. Aby potwierdzić rejestrację, kliknij w poniższy przycisk:",
+                    "Jeśli nie zakładałeś konta prosimy o zignorowanie tej wiadomości",
+                    confirmationUrl);
+            message.setContent(content, "text/html; charset=utf-8");
 
-            //send the email message
             Transport.send(message);
             System.out.println("Email Message Sent Successfully");
         } catch (MessagingException e) {
