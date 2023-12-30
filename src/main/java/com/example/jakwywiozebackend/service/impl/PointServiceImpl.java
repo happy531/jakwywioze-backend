5f18043c2706027e08744d70effbe20c6183df2c
Igor Mianowany <fimiany@gmail.com>
2023-12-30 11:55:07 +0100

    Removed expired points from filtered points

--- src/main/java/com/example/jakwywiozebackend/service/impl/PointServiceImpl.java
+++ src/main/java/com/example/jakwywiozebackend/service/impl/PointServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -125,6 +126,13 @@ public class PointServiceImpl implements PointService {
         }
         if (!filterInfoDto.isAddDynamicPoints()) {
             points.removeIf(point -> point.getDynamicPointInfo() != null);
+        }
+        else {
+            points.removeIf(point ->
+                    point.getDynamicPointInfo() != null
+                            && point.getDynamicPointInfo().getEndDate() != null
+                            && point.getDynamicPointInfo().getEndDate().isBefore(LocalDate.now())
+            );
         }
         points = points.stream()
                 .distinct()
